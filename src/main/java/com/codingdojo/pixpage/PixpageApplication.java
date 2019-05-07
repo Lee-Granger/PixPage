831a41f175c648f263eb97d79b2c58b041fc73a7
Lee Granger <Rico8719@gmail.com>
2019-05-07 14:51:34 -0700

    hello

--- src/main/java/com/codingdojo/pixpage/PixpageApplication.java
+++ src/main/java/com/codingdojo/pixpage/PixpageApplication.java
@@ -1,23 +1,16 @@
 package com.codingdojo.pixpage;
 
-import java.io.File;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.codingdojo.pixpage.controllers.FileUploadController;
 
 @SpringBootApplication
 @ComponentScan({"com.codingdojo.pixpage", "com.codingdojo.pixpage.controllers"})
 public class PixpageApplication {
 
 	public static void main(String[] args) {
-		//Will create the directory on startup if it does not exist. File() takes a String. you set the string or path 
-		//in the uploadController file.
-		new File(FileUploadController.uploadDirectory).mkdir();
-		
-		
 		SpringApplication.run(PixpageApplication.class, args);
 	}
 
--- src/main/java/com/codingdojo/pixpage/controllers/FriendManagementController.java
+++ src/main/java/com/codingdojo/pixpage/controllers/FriendManagementController.java
@@ -1,5 +1,7 @@
 package com.codingdojo.pixpage.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,10 @@ public class FriendManagementController {
 	AppService appServ;
 	
 //***************************************************************************************
+	@RequestMapping("/friend/add/{id}")
+	public String addFriend(HttpSession session) {
+		Long loggedInId = (Long) session.getAttribute("userId")
+	}
 //***************************************************************************************
 //***************************************************************************************
 //***************************************************************************************
