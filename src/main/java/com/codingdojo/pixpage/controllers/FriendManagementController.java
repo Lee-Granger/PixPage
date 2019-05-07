7e01a9588f62bb653048b67a2dfa8653a7e2b9a4
Lee Granger <Rico8719@gmail.com>
2019-05-07 14:30:22 -0700

    new controller

---
+++ src/main/java/com/codingdojo/pixpage/controllers/FriendManagementController.java
@@ -0,0 +1,20 @@
+package com.codingdojo.pixpage.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.codingdojo.pixpage.services.AppService;
+
+@Controller
+public class FriendManagementController {
+	@Autowired
+	AppService appServ;
+	
+//***************************************************************************************
+//***************************************************************************************
+//***************************************************************************************
+//***************************************************************************************
+//***************************************************************************************
+
+}
